941475882e8f5db0735d4527e3a940e75dd8a9b4
rahulbabu7 <rahulbabu436@gmail.com>
2026-07-05 03:05:51 +0530

    Update locale.nix

--- modules/locale.nix
+++ modules/locale.nix
@@ -4,6 +4,17 @@
 {
   # Set your time zone
   time.timeZone = "Asia/Kolkata";
+  services.timesyncd.enable = true;
   time.hardwareClockInLocalTime = true;
   i18n.defaultLocale = "en_US.UTF-8";
+
+  boot.blacklistedKernelModules = [ "acpi_tad" ];
+
+
+
+  # sudo flatpak override --env=TZ=Asia/Kolkata
+  # sudo flatpak override --filesystem=/etc/localtime:ro
+  # sudo flatpak override --filesystem=/etc/zoneinfo:ro
+  #
+  # Do this to get IST timing on apps
 }
